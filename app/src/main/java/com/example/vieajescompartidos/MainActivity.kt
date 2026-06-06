1e0390f7f3c14c841d818a32282dcccbd9147cf0
Dav1dCeva <davidceva0@gmail.com>
2026-06-06 15:00:35 -0500

    feat: agregar navegación entre pantallas con Navigation Compose

--- app/src/main/java/com/example/vieajescompartidos/MainActivity.kt
+++ app/src/main/java/com/example/vieajescompartidos/MainActivity.kt
@@ -5,13 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.vieajescompartidos.ui.theme.VieajesCompartidosTheme
+import com.rutashare.ui.screens.HomeScreen
+import com.rutashare.ui.screens.LoginScreen
+import com.rutashare.ui.screens.ProfileScreen
+import com.rutashare.ui.screens.PublishTripScreen
+import com.rutashare.ui.screens.RegisterScreen
+import com.rutashare.ui.screens.SearchResultsScreen
+import com.rutashare.ui.screens.TripDetailScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +27,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             VieajesCompartidosTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    AppNavigation()
                 }
             }
         }
@@ -31,17 +35,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AppNavigation() {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VieajesCompartidosTheme {
-        Greeting("Android")
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") {
+            LoginScreen(
+                onLoginClick = { navController.navigate("home") { popUpTo("login") { inclusive = true } } },
+                onRegisterClick = { navController.navigate("register") }
+            )
+        }
+
+        composable("register") {
+            RegisterScreen(
+                onRegisterClick = { navController.navigate("home") { popUpTo("login") { inclusive = true } } },
+                onLoginClick = { navController.popBackStack() }
+            )
+        }
+
+        composable("home") {
+            HomeScreen(
+                onSearchClick = { navController.navigate("search") },
+                onPublishClick = { navController.navigate("publish") },
+                onProfileClick = { navController.navigate("profile") }
+            )
+        }
+
+        composable("search") {
+            SearchResultsScreen(
+                onBackClick = { navController.popBackStack() },
+                onTripClick = { navController.navigate("trip_detail") },
+                onPublishClick = { navController.navigate("publish") },
+                onProfileClick = { navController.navigate("profile") }
+            )
+        }
+
+        composable("trip_detail") {
+            TripDetailScreen(
+                onBackClick = { navController.popBackStack() },
+                onJoinClick = { /* TODO */ },
+                onMessageClick = { /* TODO */ },
+                onPublishClick = { navController.navigate("publish") },
+                onProfileClick = { navController.navigate("profile") }
+            )
+        }
+
+        composable("publish") {
+            PublishTripScreen(
+                onBackClick = { navController.popBackStack() },
+                onPublishClick = { /* TODO */ },
+                onHomeClick = { navController.navigate("home") { popUpTo("home") { inclusive = true } } },
+                onSearchClick = { navController.navigate("search") },
+                onProfileClick = { navController.navigate("profile") }
+            )
+        }
+
+        composable("profile") {
+            ProfileScreen(
+                onHomeClick = { navController.navigate("home") { popUpTo("home") { inclusive = true } } },
+                onSearchClick = { navController.navigate("search") },
+                onPublishClick = { navController.navigate("publish") }
+            )
+        }
     }
 }
